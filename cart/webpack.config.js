cb37b264ac889c921e1e44015aacb819a6526146
Jason Law <68091773+lawtheawe@users.noreply.github.com>
2021-07-31 11:20:23 +0800

    webpack5 module federation trail

---
+++ cart/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8082,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'cart',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './CartShow': './src/bootstrap',
+      },
+      shared: ['faker'],
+      /* This setting will give warning if different version detect
+        shared: {
+          faker: {
+            singleton: true
+          }
+        }
+      */
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
